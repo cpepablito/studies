eb22018f470d3f6dd70599b8fee1b5ab35b57544
Pablo Nunes <cpepablito@gmail.com>
2020-06-17 20:35:43 -0300

    Updating studies

--- Udemy/Test/src/database/index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const config = require("../../config.json").mongoDB;
-
-mongoose.connect(config.connectionString,{ useUnifiedTopology: true, useNewUrlParser: true }).then(() => console.log(`MongoDB: ${mongoose.STATES[mongoose.connection.readyState]}`), err => console.log('error', err));
-
-mongoose.Promise = global.Promise;
-
-module.exports = mongoose;
